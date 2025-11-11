18c3cb213af7636ba52af9f914374c0f06388fb9
ArianaSolis05 <arianasolisfwd@gmail.com>
2025-11-11 15:40:39 -0600

    login

---
+++ FE/auramind_fe/src/components/Login.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "../Estilos/Login.css"
+
+function Login() {
+  return (
+    <div>
+      
+      <div class="login-container">
+  <form class="login-card">
+    <h2 class="login-title">Iniciar sesión</h2>
+    <p class="login-sub">Bienvenido de nuevo</p>
+
+    <div class="input-row">
+      <label class="input-label">
+        Correo electrónico
+        <input
+          type="email"
+          name="email"
+          class="input-field"
+          placeholder="Ingresa tu correo"
+          required
+        />
+      </label>
+    </div>
+
+    <div class="input-row">
+      <label class="input-label">
+        Contraseña
+        <input
+          type="password"
+          name="password"
+          class="input-field"
+          placeholder="Ingresa tu contraseña"
+          required
+        />
+      </label>
+    </div>
+
+    <button class="login-btn" type="button">Ingresar</button>
+
+    <p class="login-foot">
+      ¿No tienes cuenta?
+      <a href="/Formulario" class="login-link">Regístrate</a>
+    </p>
+  </form>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Login
