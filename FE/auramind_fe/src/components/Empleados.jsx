dcf9ca638f310881067e3df998aa6451daf88ede
LuisZeledon21 <lzeledon.fwd21@gmail.com>
2025-11-13 09:53:05 -0600

    empleados

---
+++ FE/auramind_fe/src/components/Empleados.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Empleados() {
+  return (
+    <div>
+      <section class="equipo">
+        <div class="card">
+          <img />
+          <h3>Ana Torres</h3>
+          <p>Psicóloga y terapeuta en Bienestar Mental</p>
+        </div>
+
+        <div class="card">
+          <img />
+          <h3>Luis Martínez</h3>
+          <p>Terapeuta y consejero en Salud Mental</p>
+        </div>
+
+        <div class="card">
+          <img />
+          <h3>Clara Sánchez</h3>
+          <p>Psicóloga clínica en Atención Psicológica</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Empleados;
